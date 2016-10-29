b01be7a7f5138f8166be98f9fbc89f1339e4dcb9
flamewing <flamewing.sonic@gmail.com>
2016-10-29 14:53:46 -0200

    Updating copyright and .gitignore.

--- src/lib/saxman.h
+++ src/lib/saxman.h
@@ -1,6 +1,7 @@
 /* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * Copyright (C) Flamewing 2013-2015 <flamewing.sonic@gmail.com>
+ * Copyright (C) Flamewing 2013-2016 <flamewing.sonic@gmail.com>
+ * Very loosely based on code by the KENS Project Development Team
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
