2b74408b7090c23ae1a770a0b270dbfce9268f9d
flamewing <flamewing.sonic@gmail.com>
2016-11-15 16:55:24 -0200

    Getting rid of autogenerated stuff. Using --foreign instead of --gnu to get rid of useless files.

--- autogen.sh
+++ autogen.sh
@@ -6,65 +6,13 @@ test -z "$srcdir" && srcdir=.
 
 DIE=0
 
-if [ -n "$GNOME2_DIR" ]; then
-	ACLOCAL_FLAGS="-I $GNOME2_DIR/share/aclocal $ACLOCAL_FLAGS"
-	LD_LIBRARY_PATH="$GNOME2_DIR/lib:$LD_LIBRARY_PATH"
-	PATH="$GNOME2_DIR/bin:$PATH"
-	export PATH
-	export LD_LIBRARY_PATH
-fi
-
-(test -f $srcdir/configure.ac) || {
-	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-	echo " top-level package directory"
-	exit 1
-}
-
+# Check for availability
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "**Error**: You must have \`autoconf' installed."
 	echo "Download the appropriate package for your distribution,"
 	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
-}
-
-(grep "^IT_PROG_INTLTOOL" $srcdir/configure.ac >/dev/null) && {
-	(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-		echo 
-		echo "**Error**: You must have \`intltool' installed."
-		echo "You can get it from:"
-		echo "	ftp://ftp.gnome.org/pub/GNOME/"
-		DIE=1
-	}
-}
-
-(grep "^AM_PROG_XML_I18N_TOOLS" $srcdir/configure.ac >/dev/null) && {
-	(xml-i18n-toolize --version) < /dev/null > /dev/null 2>&1 || {
-		echo
-		echo "**Error**: You must have \`xml-i18n-toolize' installed."
-		echo "You can get it from:"
-		echo "	ftp://ftp.gnome.org/pub/GNOME/"
-		DIE=1
-	}
-}
-
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.ac >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed."
-    echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
-    DIE=1
-  }
-}
-
-(grep "^AM_GLIB_GNU_GETTEXT" $srcdir/configure.ac >/dev/null) && {
-	(grep "sed.*POTFILES" $srcdir/configure.ac) > /dev/null || \
-	(glib-gettextize --version) < /dev/null > /dev/null 2>&1 || {
-		echo
-		echo "**Error**: You must have \`glib' installed."
-		echo "You can get it from: ftp://ftp.gtk.org/pub/gtk"
-		DIE=1
-	}
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
@@ -75,7 +23,6 @@ fi
 	NO_AUTOMAKE=yes
 }
 
-
 # if no automake, don't bother testing for aclocal
 test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
 	echo
@@ -84,6 +31,24 @@ test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
 	echo "You can get automake from ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
 }
+
+libtoolize=""
+(grep "^LT_INIT" $srcdir/configure.ac >/dev/null) && {
+	for l in glibtoolize libtoolize15 libtoolize14 libtoolize ; do
+		( $l --version < /dev/null > /dev/null 2>&1 ) && {
+			libtoolize=$l
+			break
+		}
+	done
+}
+
+if test "x$libtoolize" = "x" ; then
+	echo
+	echo "**Error**: You must have \`libtool' installed."
+	echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+fi
+
 
 if test "$DIE" -eq 1; then
 	exit 1
@@ -101,52 +66,27 @@ xlc )
 	am_opt=--include-deps;;
 esac
 
-for coin in `find $srcdir -path $srcdir/CVS -prune -o -name configure.ac -print`
-do 
-	dr=`dirname $coin`
-	if test -f $dr/NO-AUTO-GEN; then
-		echo skipping $dr -- flagged as no auto-gen
-	else
-		echo processing $dr
-		( cd $dr
+# Clean up the generated crud
+rm -f configure config.log config.guess config.sub config.cache
+rm -f libtool ltmain.sh missing mkinstalldirs install-sh
+rm -f autoconfig.h.in
+rm -f config.status aclocal.m4
+rm -f `find . -name 'Makefile.in'` `find . -name 'Makefile'`
 
-			aclocalinclude="$ACLOCAL_FLAGS"
+# touch the configure.ac file to force rebuilding configure
+touch configure.ac
 
-			if grep "^AM_GLIB_GNU_GETTEXT" configure.ac >/dev/null; then
-				echo "Creating $dr/aclocal.m4 ..."
-				test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-				echo "Running glib-gettextize...	Ignore non-fatal messages."
-				echo "no" | glib-gettextize --force --copy
-				echo "Making $dr/aclocal.m4 writable ..."
-				test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-			fi
-			if grep "^IT_PROG_INTLTOOL" configure.ac >/dev/null; then
-				echo "Running intltoolize..."
-				intltoolize --copy --force --automake
-			fi
-			if grep "^AM_PROG_XML_I18N_TOOLS" configure.ac >/dev/null; then
-				echo "Running xml-i18n-toolize..."
-				xml-i18n-toolize --copy --force --automake
-			fi
-			if grep "^LT_INIT" configure.ac >/dev/null; then
-				if test -z "$NO_LIBTOOLIZE" ; then 
-					echo "Running libtoolize..."
-					libtoolize --force --copy
-				fi
-			fi
-			echo "Running aclocal $aclocalinclude ..."
-			aclocal $aclocalinclude
-			if grep "^A[CM]_CONFIG_HEADER" configure.ac >/dev/null; then
-				echo "Running autoheader..."
-				autoheader
-			fi
-			echo "Running automake --gnu $am_opt ..."
-			automake --add-missing --copy --gnu $am_opt
-			echo "Running autoconf ..."
-			autoconf
-		)
-	fi
-done
+# Regenerate everything
+echo "Running aclocal $aclocalinclude ..."
+aclocal $aclocalincludes
+echo "Running $libtoolize..."
+$libtoolize --force --copy
+echo "Running autoheader..."
+autoheader
+echo "Running automake --foreign $am_opt ..."
+automake --add-missing --copy --foreign $am_opt
+echo "Running autoconf ..."
+autoconf 
 
 if test x$NOCONFIGURE = x; then
 	echo Running $srcdir/configure "$@" ...
