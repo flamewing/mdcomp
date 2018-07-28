flamewing/mdcomp

commit d92a31962ad0b1ad466359a612edfec13f7becc1

General LZSS improvements
Added symbol for symbolwise matches in nodes. Made nodes store the
current position, instead of destintion (the destination can be
computed from position + length anyway).

src/lib/comper.cc
@@ -152,15 +152,18 @@ public:
 		CompGraph::AdjList list = enc.find_optimal_parse();
 		CompOStream out(Dst);
 
-		size_t pos = 0;
 		// Go through each edge in the optimal path.
 		for (auto const &edge : list) {
 			switch (edge.get_type()) {
-				case EdgeType::symbolwise:
+				case EdgeType::symbolwise: {
+					auto value = edge.get_symbol();
+					size_t const high = (value >> 8) & 0xFFu,
+					             low  = (value & 0xFFu);
 					out.descbit(0);
-					out.putbyte(Data[pos]);
-					out.putbyte(Data[pos + 1]);
+					out.putbyte(high);
+					out.putbyte(low);
 					break;
+				}
 				case EdgeType::dictionary: {
 					size_t const len  = edge.get_length(),
 					             dist = edge.get_distance();
@@ -174,8 +177,6 @@ public:
 					std::cerr << "Compression produced invalid edge type " << static_cast<size_t>(edge.get_type()) << std::endl;
 					__builtin_unreachable();
 			};
-			// Go to next position.
-			pos = edge.get_dest() * 2;
 		}
 
 		// Push descriptor for end-of-file marker.

src/lib/kosinski.cc
@@ -204,19 +204,18 @@ public:
 		typename KosGraph::AdjList list = enc.find_optimal_parse();
 		KosOStream out(Dst);
 
-		size_t pos = 0;
 		// Go through each edge in the optimal path.
 		for (auto const &edge : list) {
 			switch (edge.get_type()) {
 				case EdgeType::symbolwise:
 					out.descbit(1);
-					out.putbyte(Data[pos]);
+					out.putbyte(edge.get_symbol());
 					break;
 				case EdgeType::dictionary_inline: {
-					out.descbit(0);
-					out.descbit(0);
 					size_t const len  = edge.get_length() - 2,
 					             dist = 0x100u - edge.get_distance();
+					out.descbit(0);
+					out.descbit(0);
 					out.descbit((len >> 1) & 1);
 					out.descbit(len & 1);
 					out.putbyte(dist);
@@ -224,12 +223,12 @@ public:
 				}
 				case EdgeType::dictionary_short:
 				case EdgeType::dictionary_long: {
-					out.descbit(0);
-					out.descbit(1);
 					size_t const len  = edge.get_length(),
 					             dist = 0x2000u - edge.get_distance();
-					uint16_t high = (dist >> 5) & 0xF8u,
-					         low  = (dist & 0xFFu);
+					size_t const high = (dist >> 5) & 0xF8u,
+					             low  = (dist & 0xFFu);
+					out.descbit(0);
+					out.descbit(1);
 					if (edge.get_type() == EdgeType::dictionary_short) {
 						out.putbyte(low);
 						out.putbyte(high | (len - 2));
@@ -245,8 +244,6 @@ public:
 					std::cerr << "Compression produced invalid edge type " << static_cast<size_t>(edge.get_type()) << std::endl;
 					__builtin_unreachable();
 			}
-			// Go to next position.
-			pos = edge.get_dest();
 		}
 
 		// Push descriptor for end-of-file marker.

src/lib/kosplus.cc
@@ -201,19 +201,18 @@ public:
 		typename KosGraph::AdjList list = enc.find_optimal_parse();
 		KosOStream out(Dst);
 
-		size_t pos = 0;
 		// Go through each edge in the optimal path.
 		for (auto const &edge : list) {
 			switch (edge.get_type()) {
 				case EdgeType::symbolwise:
 					out.descbit(1);
-					out.putbyte(Data[pos]);
+					out.putbyte(edge.get_symbol());
 					break;
 				case EdgeType::dictionary_inline: {
-					out.descbit(0);
-					out.descbit(0);
 					size_t const len  = edge.get_length() - 2,
 					             dist = 0x100u - edge.get_distance();
+					out.descbit(0);
+					out.descbit(0);
 					out.putbyte(dist);
 					out.descbit((len >> 1) & 1);
 					out.descbit(len & 1);
@@ -221,12 +220,12 @@ public:
 				}
 				case EdgeType::dictionary_short:
 				case EdgeType::dictionary_long: {
-					out.descbit(0);
-					out.descbit(1);
 					size_t const len  = edge.get_length(),
 					             dist = 0x2000u - edge.get_distance();
-					uint16_t high = (dist >> 5) & 0xF8u,
-					         low  = (dist & 0xFFu);
+					size_t high = (dist >> 5) & 0xF8u,
+					       low  = (dist & 0xFFu);
+					out.descbit(0);
+					out.descbit(1);
 					if (edge.get_type() == EdgeType::dictionary_short) {
 						out.putbyte(high | (10 - len));
 						out.putbyte(low);
@@ -242,8 +241,6 @@ public:
 					std::cerr << "Compression produced invalid edge type " << static_cast<size_t>(edge.get_type()) << std::endl;
 					__builtin_unreachable();
 			}
-			// Go to next position.
-			pos = edge.get_dest();
 		}
 
 		// Push descriptor for end-of-file marker.

src/lib/lzss.hh
@@ -44,62 +44,64 @@
  * in the [u, v) range (half-open) -- that is, node v is not part of the match.
  * Each node is a character in the file, and is represented by its position.
  */
-template <typename EdgeType>
+template <typename Adaptor>
 class AdjListNode {
+public:
+	using EdgeType = typename Adaptor::EdgeType;
+	using stream_t = typename Adaptor::stream_t;
+
 private:
 	// The first character after the match ends.
-	size_t destnode;
+	size_t currpos;
 	// Cost, in bits, of "covering" all of the characters in the match.
 	size_t weight;
-	// How many characters back does the match begin at.
-	size_t distance;
-	// How long the match is.
-	size_t length;
 	EdgeType type;
+	union {
+		struct {
+			// How many characters back does the match begin at.
+			size_t distance;
+			// How long the match is.
+			size_t length;
+		} match;
+		stream_t symbol;
+	};
 public:
 	// Constructors.
 	constexpr AdjListNode() noexcept
-		: destnode(1), weight(std::numeric_limits<size_t>::max()), distance(1),
-		  length(1), type(EdgeType::symbolwise) {
+		: currpos(0), weight(std::numeric_limits<size_t>::max()),
+		  type(EdgeType::symbolwise), symbol(stream_t(0)) {
 	}
-	constexpr AdjListNode(size_t dest, size_t dist, size_t len, size_t wgt, EdgeType ty) noexcept
-		: destnode(dest), weight(wgt), distance(dist), length(len), type(ty) {
+	constexpr AdjListNode(size_t pos, stream_t sym, EdgeType ty) noexcept
+		: currpos(pos), weight(Adaptor::edge_weight(ty)), type(ty), symbol(sym) {
 	}
-	constexpr AdjListNode(AdjListNode<EdgeType> const &other) noexcept = default;
-	constexpr AdjListNode(AdjListNode<EdgeType> &&other) noexcept = default;
-	constexpr AdjListNode &operator=(AdjListNode<EdgeType> const &other) noexcept = default;
-	constexpr AdjListNode &operator=(AdjListNode<EdgeType> &&other) noexcept = default;
+	constexpr AdjListNode(size_t pos, size_t dist, size_t len, size_t wgt, EdgeType ty) noexcept
+		: currpos(pos), weight(wgt), type(ty), match({dist, len}) {
+	}
+	constexpr AdjListNode(AdjListNode<Adaptor> const &other) noexcept = default;
+	constexpr AdjListNode(AdjListNode<Adaptor> &&other) noexcept = default;
+	constexpr AdjListNode &operator=(AdjListNode<Adaptor> const &other) noexcept = default;
+	constexpr AdjListNode &operator=(AdjListNode<Adaptor> &&other) noexcept = default;
 	// Getters.
+	constexpr size_t get_pos() const noexcept {
+		return currpos;
+	}
 	constexpr size_t get_dest() const noexcept {
-		return destnode;
+		return currpos + get_length();
 	}
 	constexpr size_t get_weight() const noexcept {
 		return weight;
 	}
 	constexpr size_t get_distance() const noexcept {
-		return distance;
+		return type == EdgeType::symbolwise ? 0 : match.distance;
 	}
 	constexpr size_t get_length() const noexcept {
-		return length;
+		return type == EdgeType::symbolwise ? 1 : match.length;
+	}
+	constexpr stream_t get_symbol() const noexcept {
+		return type == EdgeType::symbolwise ? symbol : stream_t(-1);
 	}
 	constexpr EdgeType get_type() const noexcept {
 		return type;
-	}
-	// Comparison operator. Lowest weight first, on tie, break by shortest
-	// length, on further tie break by distance. Used only on the multimap.
-	constexpr bool operator<(AdjListNode<EdgeType> const &other) const noexcept {
-		if (weight < other.weight) {
-			return true;
-		} else if (weight > other.weight) {
-			return false;
-		}
-		if (length < other.length) {
-			return true;
-		} else if (length > other.length) {
-			return false;
-		} else {
-			return distance < other.distance;
-		}
 	}
 };
 
@@ -154,8 +156,8 @@ template<typename Adaptor>
 class LZSSGraph {
 public:
 	using EdgeType = typename Adaptor::EdgeType;
-	using Node_t = AdjListNode<EdgeType>;
 	using stream_t = typename Adaptor::stream_t;
+	using Node_t = AdjListNode<Adaptor>;
 	using AdjList = std::list<Node_t>;
 	using MatchVector = std::vector<Node_t>;
 private:
@@ -184,7 +186,7 @@ private:
 		MatchVector matches(ubound);
 		// Start with the literal/symbolwise encoding of the current node.
 		EdgeType const ty = Adaptor::match_type(0, 1);
-		matches[0] = Node_t(basenode + 1, 0, 1, Adaptor::edge_weight(ty), ty);
+		matches[0] = Node_t(basenode, data[basenode], ty);
 		// Get extra dictionary matches dependent on specific encoder.
 		static_assert(noexcept(Adaptor::extra_matches(data, basenode, ubound, lbound, matches)),
 		                       "Adaptor::extra_matches() is not noexcept");
@@ -206,7 +208,7 @@ private:
 					size_t const wgt = Adaptor::edge_weight(ty);
 					Node_t &best = matches[jj - 1];
 					if (wgt < best.get_weight()) {
-						best = Node_t(basenode + jj, basenode - ii, jj, wgt, ty);
+						best = Node_t(basenode, basenode - ii, jj, wgt, ty);
 					}
 				}
 				// We can find no more matches with the current starting node.

src/lib/rocket.cc
@@ -200,19 +200,19 @@ public:
 		RockGraph::AdjList list = enc.find_optimal_parse();
 		RockOStream out(Dst);
 
-		size_t pos = 0;
 		// Go through each edge in the optimal path.
 		for (auto const &edge : list) {
 			switch (edge.get_type()) {
 				case EdgeType::symbolwise:
 					out.descbit(1);
-					out.putbyte(Data[pos]);
+					out.putbyte(edge.get_symbol());
 					break;
 				case EdgeType::dictionary: {
 					size_t const len  = edge.get_length(),
-					             dist = edge.get_distance();
+					             dist = edge.get_distance(),
+					             pos  = edge.get_pos();
 					out.descbit(0);
-					RockIndex const index{(0x3C0 + pos - dist)};
+					RockIndex const index{(0x3C0u + pos - dist)};
 					out.putbyte(((len-1)<<2)|(index>>8));
 					out.putbyte(index);
 					break;
@@ -222,8 +222,6 @@ public:
 					std::cerr << "Compression produced invalid edge type " << static_cast<size_t>(edge.get_type()) << std::endl;
 					__builtin_unreachable();
 			};
-			// Go to next position.
-			pos = edge.get_dest();
 		}
 	}
 };

src/lib/saxman.cc
@@ -106,6 +106,7 @@ class saxman_internal {
 		constexpr static void extra_matches(stream_t const *data, size_t const basenode,
 			                                size_t const ubound, size_t const lbound,
 			                                LZSSGraph<SaxmanAdaptor>::MatchVector &matches) noexcept {
+			using Node_t = LZSSGraph<SaxmanAdaptor>::Node_t;
 			ignore_unused_variable_warning(lbound);
 			// Can't encode zero match after this point.
 			if (basenode >= SearchBufSize-1) {
@@ -122,9 +123,9 @@ class saxman_internal {
 			if (jj >= 3) {
 				// Got them, so add them to the list.
 				EdgeType const ty = EdgeType::zerofill;
-				matches[jj - 1] = AdjListNode<EdgeType>(basenode + jj,
-					                          numeric_limits<size_t>::max(),
-					                          jj, edge_weight(ty), ty);
+				matches[jj - 1] = Node_t(basenode,
+					                     numeric_limits<size_t>::max(),
+					                     jj, edge_weight(ty), ty);
 			}
 		}
 		// Saxman needs no additional padding at the end-of-file.
@@ -162,7 +163,7 @@ public:
 				// Length is low 4 bits plus 3.
 				length = (length & 0xF) + 3;
 				// And there is an additional 0x12 bytes added to offset.
-				offset = (offset + 0x12) & 0xFFF;
+				offset = (offset + 0x12) % SaxmanAdaptor::SearchBufSize;
 				// The offset is stored as being absolute within current 0x1000-byte
 				// block, with part of it being remapped to the end of the previous
 				// 0x1000-byte block. We just rebase it around basedest.
@@ -199,23 +200,22 @@ public:
 		SaxGraph::AdjList list = enc.find_optimal_parse();
 		SaxOStream out(Dst);
 
-		size_t pos = 0;
 		// Go through each edge in the optimal path.
 		for (auto const &edge : list) {
 			switch (edge.get_type()) {
 				case EdgeType::symbolwise:
 					out.descbit(1);
-					out.putbyte(Data[pos]);
+					out.putbyte(edge.get_symbol());
 					break;
 				case EdgeType::dictionary:
 				case EdgeType::zerofill: {
 					size_t const len  = edge.get_length(),
-					             dist = edge.get_distance();
+					             dist = edge.get_distance(),
+					             pos  = edge.get_pos();
+					size_t const base = (pos - dist - 0x12u) & 0xFFFu;
+					size_t const low  = base & 0xFFu,
+					             high = ((len - 3u) & 0x0Fu) | ((base >> 4) & 0xF0u);
 					out.descbit(0);
-					size_t low = pos - dist, high = len;
-					low = (low - 0x12) & 0xFFF;
-					high = ((high - 3) & 0x0F) | ((low >> 4) & 0xF0);
-					low &= 0xFF;
 					out.putbyte(low);
 					out.putbyte(high);
 					break;
@@ -225,7 +225,6 @@ public:
 					std::cerr << "Compression produced invalid edge type " << static_cast<size_t>(edge.get_type()) << std::endl;
 					__builtin_unreachable();
 			};
-			pos = edge.get_dest();
 		}
 	}
 };

commit d3896209a642cbd980edd862ce387af4d2829e4b

Fixing non-character-based LZSS (notably, comper)
Broken by d92a31962ad0b1ad.

src/lib/comper.cc
@@ -38,6 +38,7 @@ class comper_internal {
 	// NOTE: This has to be changed for other LZSS-based compression schemes.
 	struct ComperAdaptor {
 		using stream_t = uint16_t;
+		using stream_endian_t = BigEndian;
 		using descriptor_t = uint16_t;
 		using descriptor_endian_t = BigEndian;
 		enum class EdgeType : size_t {

src/lib/kosinski.cc
@@ -38,6 +38,7 @@ class kosinski_internal {
 	// NOTE: This has to be changed for other LZSS-based compression schemes.
 	struct KosinskiAdaptor {
 		using stream_t = uint8_t;
+		using stream_endian_t = BigEndian;
 		using descriptor_t = uint16_t;
 		using descriptor_endian_t = LittleEndian;
 		enum class EdgeType : size_t {

src/lib/kosplus.cc
@@ -38,6 +38,7 @@ class kosplus_internal {
 	// NOTE: This has to be changed for other LZSS-based compression schemes.
 	struct KosPlusAdaptor {
 		using stream_t = uint8_t;
+		using stream_endian_t = BigEndian;
 		using descriptor_t = uint8_t;
 		using descriptor_endian_t = LittleEndian;
 		enum class EdgeType : size_t {

src/lib/lzss.hh
@@ -110,8 +110,8 @@ class SlidingWindow {
 public:
 	using EdgeType = typename Adaptor::EdgeType;
 	using stream_t = typename Adaptor::stream_t;
+	using stream_endian_t = typename Adaptor::stream_endian_t;
 	using Node_t = AdjListNode<Adaptor>;
-	using AdjList = std::list<Node_t>;
 	using MatchVector = std::vector<Node_t>;
 
 	SlidingWindow(uint8_t const *dt, size_t const size) noexcept
@@ -160,8 +160,12 @@ public:
 		static_assert(noexcept(Adaptor::extra_matches(data, basenode, ubound, lbound, matches)),
 		                       "Adaptor::extra_matches() is not noexcept");
 		// Start with the literal/symbolwise encoding of the current node.
-		EdgeType const ty = Adaptor::match_type(0, 1);
-		matches[0] = Node_t(basenode, data[basenode], ty);
+		{
+			EdgeType const ty = Adaptor::match_type(0, 1);
+			auto ptr = reinterpret_cast<const uint8_t*>(data + basenode);
+			stream_t val = stream_endian_t::template ReadN<decltype(ptr), sizeof(stream_t)>(ptr);
+			matches[0] = Node_t(basenode, val, ty);
+		}
 		// Get extra dictionary matches dependent on specific encoder.
 		Adaptor::extra_matches(data, basenode, ubound, lbound, matches);
 		// First node is special.
@@ -212,6 +216,7 @@ private:
  * members:
  *  struct LZSSAdaptor {
  *  	using stream_t     = uint8_t;
+ *  	using stream_endian_t = BigEndian;
  *  	using descriptor_t = uint16_t;
  *  	using descriptor_endian_t = LittleEndian;
  *  	enum class EdgeType : size_t {

src/lib/rocket.cc
@@ -39,6 +39,7 @@ struct rocket_internal {
 	// NOTE: This has to be changed for other LZSS-based compression schemes.
 	struct RocketAdaptor {
 		using stream_t = uint8_t;
+		using stream_endian_t = BigEndian;
 		using descriptor_t = uint8_t;
 		using descriptor_endian_t = LittleEndian;
 		enum class EdgeType : size_t {

src/lib/saxman.cc
@@ -39,6 +39,7 @@ class saxman_internal {
 	// NOTE: This has to be changed for other LZSS-based compression schemes.
 	struct SaxmanAdaptor {
 		using stream_t = uint8_t;
+		using stream_endian_t = BigEndian;
 		using descriptor_t = uint8_t;
 		using descriptor_endian_t = LittleEndian;
 		enum class EdgeType : size_t {