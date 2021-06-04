59d8c3e4c32f3e521a09dc199b66a485716e2d99
vladikcomper <rubstov@gmail.com>
2021-06-04 21:08:55 +0300

    Introduce Comper-X compression format and toolset.

---
+++ include/mdcomp/comperx.hh
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) Flamewing 2013-2016 <flamewing.sonic@gmail.com>
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIB_COMPERX_HH
+#define LIB_COMPERX_HH
+
+#include <mdcomp/basic_decoder.hh>
+#include <mdcomp/moduled_adaptor.hh>
+
+#include <iosfwd>
+
+class comperx;
+using basic_comperx   = BasicDecoder<comperx, PadMode::PadEven>;
+using moduled_comperx = ModuledAdaptor<comperx, 4096U, 1U>;
+
+class comperx : public basic_comperx, public moduled_comperx {
+    friend basic_comperx;
+    friend moduled_comperx;
+    static bool encode(std::ostream& Dst, uint8_t const* data, size_t Size);
+
+public:
+    using basic_comperx::encode;
+    static bool decode(std::istream& Src, std::iostream& Dst);
+};
+
+#endif    // LIB_COMPERX_HH
---
+++ src/tools/comperx.cc
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) Flamewing 2013-2015 <flamewing.sonic@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <getopt.h>
+#include <mdcomp/comperx.hh>
+
+#include <array>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using std::cerr;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::ios;
+using std::ofstream;
+using std::stringstream;
+
+static void usage(char* prog) {
+    cerr << "Usage: " << prog
+         << " [-c|--crunch|-x|--extract=[{pointer}]] [-m|--moduled] "
+            "{input_filename} "
+            "{output_filename}"
+         << endl;
+    cerr << endl;
+    cerr << "\t-x,--extract\tExtract from {pointer} address in file." << endl;
+    cerr << "\t-c,--crunch \tAssume input file is Comper-compressed and "
+            "recompress to output file."
+         << endl
+         << "\t            \tIf --chunch is in effect, a missing "
+            "output_filename means recompress"
+         << endl
+         << "\t            \tto input_filename." << endl;
+    cerr << "\t-m,--moduled\tUse compression in modules of 4096 bytes." << endl;
+}
+
+int main(int argc, char* argv[]) {
+    static constexpr const std::array<option, 4> long_options{
+            option{"extract", optional_argument, nullptr, 'x'},
+            option{"moduled", no_argument, nullptr, 'm'},
+            option{"crunch", no_argument, nullptr, 'c'},
+            option{nullptr, 0, nullptr, 0}};
+
+    bool   extract = false;
+    bool   moduled = false;
+    bool   crunch  = false;
+    size_t pointer = 0;
+
+    while (true) {
+        int option_index = 0;
+        int c            = getopt_long(
+                argc, argv, "x::mc", long_options.data(), &option_index);
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+        case 'x':
+            extract = true;
+            if (optarg != nullptr) {
+                pointer = strtoul(optarg, nullptr, 0);
+            }
+            break;
+        case 'c':
+            crunch = true;
+            break;
+        case 'm':
+            moduled = true;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if ((!crunch && argc - optind < 2) || (crunch && argc - optind < 1)) {
+        usage(argv[0]);
+        return 1;
+    }
+
+    if (extract && crunch) {
+        cerr << "Error: --extract and --crunch can't be used at the same time."
+             << endl
+             << endl;
+        return 4;
+    }
+
+    const char* outfile
+            = crunch && argc - optind < 2 ? argv[optind] : argv[optind + 1];
+
+    ifstream fin(argv[optind], ios::in | ios::binary);
+    if (!fin.good()) {
+        cerr << "Input file '" << argv[optind] << "' could not be opened."
+             << endl
+             << endl;
+        return 2;
+    }
+
+    if (crunch) {
+        stringstream buffer(ios::in | ios::out | ios::binary);
+        fin.seekg(pointer);
+        comperx::decode(fin, buffer);
+        fin.close();
+        buffer.seekg(0);
+
+        fstream fout(outfile, ios::in | ios::out | ios::binary | ios::trunc);
+        if (!fout.good()) {
+            cerr << "Output file '" << argv[optind + 1]
+                 << "' could not be opened." << endl
+                 << endl;
+            return 3;
+        }
+        if (moduled) {
+            comperx::moduled_encode(buffer, fout);
+        } else {
+            comperx::encode(buffer, fout);
+        }
+    } else {
+        fstream fout(outfile, ios::in | ios::out | ios::binary | ios::trunc);
+        if (!fout.good()) {
+            cerr << "Output file '" << argv[optind + 1]
+                 << "' could not be opened." << endl
+                 << endl;
+            return 3;
+        }
+
+        if (extract) {
+            fin.seekg(pointer);
+            if (moduled) {
+                comperx::moduled_decode(fin, fout);
+            } else {
+                comperx::decode(fin, fout);
+            }
+        } else {
+            if (moduled) {
+                comperx::moduled_encode(fin, fout);
+            } else {
+                comperx::encode(fin, fout);
+            }
+        }
+    }
+
+    return 0;
+}
