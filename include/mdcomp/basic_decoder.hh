34bbe78090395bf484d50f7822179517880ae0a4
flamewing <flamewing.sonic@gmail.com>
2023-10-16 18:14:55 +0200

    Safely muting data alignment warning
    For now. I need to ensure this workaround is not needed.

--- include/mdcomp/basic_decoder.hh
+++ include/mdcomp/basic_decoder.hh
@@ -20,6 +20,7 @@
 #ifndef LIB_BASIC_DECODER_H
 #define LIB_BASIC_DECODER_H
 
+#include "mdcomp/ignore_unused_variable_warning.hh"
 #include "mdcomp/stream_utils.hh"
 
 #include <cstddef>
@@ -41,6 +42,49 @@ public:
     static void extract(std::istream& source, std::iostream& dest);
 };
 
+template <typename T, size_t Align = alignof(std::max_align_t)>
+class aligned_allocator {
+private:
+    static_assert(
+            Align >= alignof(T),
+            "Beware that types like int have minimum alignment requirements "
+            "or access will result in crashes.");
+
+public:
+    using value_type = T;
+
+    template <typename U>
+    struct rebind {
+        using other = aligned_allocator<U, Align>;
+    };
+
+    constexpr aligned_allocator() noexcept                         = default;
+    constexpr aligned_allocator(aligned_allocator const&) noexcept = default;
+    constexpr aligned_allocator(aligned_allocator&&) noexcept      = default;
+    constexpr ~aligned_allocator() noexcept                        = default;
+
+    constexpr aligned_allocator& operator=(aligned_allocator const&) noexcept = default;
+    constexpr aligned_allocator& operator=(aligned_allocator&&) noexcept      = default;
+
+    template <typename U>
+    explicit constexpr aligned_allocator(aligned_allocator<U, Align> const&) noexcept {}
+
+    [[nodiscard]] T* allocate(std::size_t count) {
+        if (count > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+            throw std::bad_array_new_length();
+        }
+
+        auto const size_bytes = count * sizeof(T);
+        return reinterpret_cast<T*>(
+                ::operator new[](size_bytes, std::align_val_t{Align}));
+    }
+
+    void deallocate(T* pointer, [[maybe_unused]] std::size_t count_bytes) {
+        ignore_unused_variable_warning(count_bytes);
+        ::operator delete[](pointer, std::align_val_t{Align});
+    }
+};
+
 template <typename Format, pad_mode Pad, typename... Args>
 bool basic_decoder<Format, Pad, Args...>::encode(
         std::istream& source, std::ostream& dest, Args... args) {
@@ -48,7 +92,7 @@ bool basic_decoder<Format, Pad, Args...>::encode(
     source.ignore(std::numeric_limits<std::streamsize>::max());
     auto full_size = static_cast<size_t>(source.gcount());
     source.seekg(start);
-    std::vector<uint8_t> data;
+    std::vector<uint8_t, aligned_allocator<uint8_t>> data;
     if constexpr (Pad == pad_mode::pad_even) {
         data.resize(detail::round_up(full_size, 2U));
     } else {
